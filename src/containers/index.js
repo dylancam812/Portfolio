5e6039428b3533e2f4d1f359a991776daa90d1e5
Dylan Camacho <dylancam812@gmail.com>
2025-08-20 03:03:29 -0700

    Updating portfolio with UI improvements and replacing skills with new about section

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Projects } from './projects/Projects';
-export { default as Skills } from './skills/Skills';
-export { default as Home } from './home/Home';
-export { default as Contact } from './contact/Contact';
